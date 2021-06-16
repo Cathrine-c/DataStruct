282a6fc88beab83bd4a70efac8a5edc5e5e68a9b
Cathrine-c <1730004028@qq.com>
2021-06-16 23:12:52 +0800

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -20,11 +20,38 @@ public class Test1 {
 
         for (int i=2;i<=n;i++){
             dp[i] = Math.max(dp[i-1],nums[i-1]+dp[i-2]);
-
         }
-
         return dp[n];
-
-
     }
+
+
+    //给定一个 m × n 大小的非负整数矩阵，求从左上角开始到右下角结束的、经过的数字的和最
+    //小的路径。每次只能向右或者向下移动。
+    public int minPathSum(int[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+
+                }else if (i==0){
+                    dp[i][j] = dp[i][j-1]+grid[i][j];
+
+                } else if (j == 0) {
+                    dp[i][j] = dp[i-1][j]+grid[i][j];
+
+                }else {
+                    dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+
+
 }
